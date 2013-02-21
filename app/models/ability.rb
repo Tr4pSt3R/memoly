5eb23445cdc23ebfd56596fc0941f8f627a1f428
Jones Agyemang <jones.k.agyemang@gmail.com>
2013-02-21 19:29:47 +0000

    Remove Abilities restrictions for AlphaUser

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,10 @@ class Ability
 
   def initialize(user)
     # alias_action :show => :view
-    user ||= User.new # guest user (not logged in)
-    if user
-        can [:create], AlphaUser
-        can [:show], AlphaUser
-    end
+    # user ||= User.new # guest user (not logged in)
+    # if user
+    #     can [:create], AlphaUser
+    #     can [:show], AlphaUser
+    # end
   end
 end
