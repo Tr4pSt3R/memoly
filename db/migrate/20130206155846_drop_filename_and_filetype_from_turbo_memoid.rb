4efda013491e8cef8a6a063c69fb02ed21bce77e
Jones Agyemang <jones.k.agyemang@gmail.com>
2013-03-07 22:28:51 +0000

    Remove migration script

--- db/migrate/20130206155846_drop_filename_and_filetype_from_turbo_memoid.rb
+++
@@ -1,6 +0,0 @@
-class DropFilenameAndFiletypeFromTurboMemoid < ActiveRecord::Migration
-  def change
-  	remove_column :turbo_memoids, :filename, :string
-  	remove_column :turbo_memoids, :filetype, :string
-  end
-end
