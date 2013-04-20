b587f4d5057ddceb1e0089d365effa9246cc4684
Jones Agyemang <jones.k.agyemang@gmail.com>
2013-04-20 20:51:15 +0100

    Add static content pages for Privacy, Terms and Contact Us. Add SWLondon

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,17 @@ module Memoly
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Use RSpec to generate starter files for our application's test suite
+    config.generators do |g|
+        g.test_frameork :rspec, 
+            :fixtures => true, 
+            :view_specs => false, 
+            :helper_specs => false, 
+            :routing_specs => false, 
+            :controller_specs => true, 
+            :request_specs => true
+        g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
