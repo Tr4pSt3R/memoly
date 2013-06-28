2f90966bb5dedd99b357a81c670f92b59e96dbd7
Jones Agyemang <jones.k.agyemang@gmail.com>
2013-06-28 11:34:11 +0100

    Remove TurboMemoid drop migration. Aesthetic changes to Thumbnail outline

--- db/migrate/20130206155846_drop_filename_and_filetype_from_turbo_memoid.rb
+++ db/migrate/20130206155846_drop_filename_and_filetype_from_turbo_memoid.rb
@@ -1,6 +1,6 @@
 class DropFilenameAndFiletypeFromTurboMemoid < ActiveRecord::Migration
   def change
-  	remove_column :turbo_memoids, :filename, :string
-  	remove_column :turbo_memoids, :filetype, :string
+  	# remove_column :turbo_memoids, :filename, :string
+  	# remove_column :turbo_memoids, :filetype, :string
   end
 end
