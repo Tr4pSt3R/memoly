21f66fa69a34aebb5392cb625414de80458d58b9
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-01-11 14:53:01 +0000

    Battling with Capistrano again

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,21 +1,4 @@
-set :rails_env, 'staging'
-server '162.243.25.180',  :app, :web, :db, :primary => true
+server "162.243.25.180", :app, :web, :db, :primary => true
+set :deploy_to, "/var/www/memoly_staging"
 
-set :user,          "root"   
-set :branch, 		"memoly_core"
-
-set :deploy_to, "/home/rails/"
-
-# Bundler
-set :bundle_without,  [:development, :test]
-
-# Config files
-# set :config_files, %w(database.yml settings.yml)
-
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
+set :branch, "memoly_core"
