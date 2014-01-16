9c3359c434aeea0f94795f2c3f972262e25663f6
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-01-16 21:20:36 +0000

    Setup DB if migration fails

--- config/deploy.rb
+++ config/deploy.rb
@@ -79,7 +79,7 @@ namespace :deploy do
     begin
       run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} db:migrate"
     rescue
-      run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} db:schema:load"
+      run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} db:setup"
     end
   end
 
