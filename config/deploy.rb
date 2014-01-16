cd4fab35aab0b3c2dc4bfd8cd410455b4cc74da1
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-01-16 20:41:05 +0000

    Precompile assets locally first

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,15 +40,41 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
-  after "deploy:migrate", "deploy:precompile_assets"
-  task :precompile_assets do
-    puts "Rails Env: #{rails_env}"
-    run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} assets:precompile:primary"
+  # Precompile Assets Locally for CapDeploy
+  # http://www.rostamizadeh.net/blog/2012/04/14/precompiling-assets-locally-for-capistrano-deployment
+  before 'deploy:finalize_update', 'deploy:assets:symlink'
+  after 'deploy:update_code', 'deploy:assets:precompile'
+  namespace :assets do
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
+
+    task :symlink, roles: :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+            mkdir -p #{latest_release}/public &&
+            mkdir -p #{shared_path}/assets &&
+            ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    end
   end
+
 
   desc ""
   task :migrate do 
-    run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} db:schema:load"
+    begin
+      run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} db:migrate"
+    rescue
+      run "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} db:schema:load"
+    end
   end
 
   task :symlink_config, roles: :app do 
